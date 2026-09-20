d926a2feb432e23f810807a046f9eca40e12ba24
qzj1472 <qzj1472@users.noreply.github.com>
2026-09-20 14:31:02 +0800

    build(deps): add commons-compress for voice model archives

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
     compileOnly(files("libs/xposed-api-stub.jar"))
+    implementation("org.apache.commons:commons-compress:1.26.2")
 }
+
+
 
 
